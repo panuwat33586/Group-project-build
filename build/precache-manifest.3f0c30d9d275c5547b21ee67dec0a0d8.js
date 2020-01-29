1ce51d152d820d6ff2b2ce028998297a02916f73
panuwat33586 <panuwat33586@gmail.com>
2020-01-29 18:13:21 +0700

    build

---
+++ build/precache-manifest.3f0c30d9d275c5547b21ee67dec0a0d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006ce15389e955501db236cd636c68f0",
+    "url": "/Phawit-net/Massage_team_project_Frontend/index.html"
+  },
+  {
+    "revision": "065d36ae06b06ab7b184",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "92a9483989c35f3ed21d",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/css/main.4bb0cdd6.chunk.css"
+  },
+  {
+    "revision": "065d36ae06b06ab7b184",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/js/2.5e3060fe.chunk.js"
+  },
+  {
+    "revision": "113b15946dc698d5a61446ea2c22067f",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/js/2.5e3060fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92a9483989c35f3ed21d",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/js/main.5dc0c979.chunk.js"
+  },
+  {
+    "revision": "1643a20bfb15d1506c89",
+    "url": "/Phawit-net/Massage_team_project_Frontend/static/js/runtime-main.8fa72884.js"
+  }
+]);
---
+++ build/precache-manifest.b33851a8bfdf2de5da918b2b700e4626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170275ea6ffbf8658c6d1ce70ec0f010",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce13f86f4d3dab3db62d",
+    "url": "./static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "4e18ef2495e421586d44",
+    "url": "./static/css/main.4ed97162.chunk.css"
+  },
+  {
+    "revision": "ce13f86f4d3dab3db62d",
+    "url": "./static/js/2.87dbcde8.chunk.js"
+  },
+  {
+    "revision": "113b15946dc698d5a61446ea2c22067f",
+    "url": "./static/js/2.87dbcde8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e18ef2495e421586d44",
+    "url": "./static/js/main.e587fe68.chunk.js"
+  },
+  {
+    "revision": "4e32fe94d600575979da",
+    "url": "./static/js/runtime-main.9c88c5e6.js"
+  }
+]);
